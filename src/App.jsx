aa0e46cbf0d7361b53e8d2b97b3d4eb1b825eb5f
lucilene253 <lucylenecoelho@hotmail.com>
2026-04-16 10:40:59 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,10 @@ function App() {
 
       <main>
         <section id="sobre">
-          <h2>Sobre Mim</h2>
+         <dic>
+           <img src="/WhatsApp Image 2026-04-10 at 11.14.09.jpeg" width="50px" />
+          <h2>Sobre Mim</h2> 
+         </div>
           <p>
             Sou apaixonada por tecnologia, ensino e inovação. Gosto de aprender e compartilhar conhecimento.
           </p>
